39b83fb0364872a991280fc304973dfd0aadb89c
pudgypoultry <50252390+pudgypoultry@users.noreply.github.com>
2023-05-19 21:48:47 -0700

    Snap functionality added!
    Cards now snap to boxes, every Interactable object has a valid list of things it can snap to and a valid list of things that can snap to it. Additionally, snapping to a box makes a card a child of that box. Picking the card up makes the original parent the parent of the card.

---
+++ Assets/Scripts/BoardInteractions/Interactable.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    private Vector3 targetPosition;
+    private float originalHeight;
+    private float slottedHeight;
+    private GameObject potentialSlot;
+    private GameObject currentSlot;
+    private Transform originalParent;
+    private bool slotted = false;
+
+    [SerializeField] private float heightOffset = 0;
+    [SerializeField] private float dragDelay = 100;
+    [SerializeField] private List<string> validBoxes;
+    [SerializeField] private List<string> boxTypes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        targetPosition = transform.position;
+        slottedHeight = transform.position.y + heightOffset;
+        originalHeight = transform.position.y;
+        originalParent = transform.parent;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.position = new Vector3(targetPosition.x, transform.position.y, targetPosition.z);
+        if (!slotted)
+        {
+            LerpToTarget();
+        }
+
+    }
+
+    private void LerpToTarget()
+    {
+
+        if (slotted)
+        {
+            ChangeTarget(currentSlot.transform.parent.position);
+        }
+
+        // targetPosition.x = Mathf.Lerp(transform.position.x, targetPosition.x, dragDelay);
+        // targetPosition.z = Mathf.Lerp(transform.position.z, targetPosition.z, dragDelay);
+
+        float x = Mathf.Lerp(transform.position.x, targetPosition.x, dragDelay);
+        float z = Mathf.Lerp(transform.position.z, targetPosition.z, dragDelay);
+
+        transform.position = new Vector3(x, transform.position.y, z);
+    }
+
+    public void ChangeTarget(Vector3 newTarget)
+    {
+        targetPosition = newTarget;
+        // Debug.Log("I am moving from " + transform.position + "to " + targetPosition + "!");
+    }
+
+    public void PickUp(Vector3 pos)
+    {
+        currentSlot = null;
+        slotted = false;
+        transform.position = new Vector3(transform.position.x, pos.y, transform.position.z);
+
+        if (transform.parent != originalParent)
+        { 
+            transform.parent = originalParent; 
+        }
+    }
+
+    public void SetDown(Vector3 pos)
+    {
+        // ADD RULES HERE FOR WHEN WE'RE ALLOWED TO SLOT
+        if (potentialSlot != null)
+        {
+            foreach (string boxType in validBoxes)
+            {
+                if (potentialSlot.transform.parent.GetComponent<Interactable>().boxTypes.Contains(boxType))
+                {
+                    currentSlot = potentialSlot;
+                    Debug.Log(currentSlot.transform.parent);
+                    potentialSlot = null;
+                    slotted = true;
+                    break;
+                }
+            }
+        }
+        
+        if (slotted)
+        {
+            targetPosition = new Vector3(currentSlot.transform.parent.position.x, slottedHeight, currentSlot.transform.parent.position.z);
+            transform.position = targetPosition;
+            transform.parent = currentSlot.transform;
+        }
+
+        else 
+        {
+            transform.position = new Vector3(pos.x, originalHeight, pos.z);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        Debug.Log(collision);
+        if (collision.gameObject.CompareTag("Slot"))
+        {
+            // Debug.Log("THIS IS MY HOLE IT WAS MADE FOR ME");
+            potentialSlot = collision.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider collision)
+    {
+        if (collision.gameObject.CompareTag("Slot"))
+        {
+            // Debug.Log("THIS IS MY HOLE IT WAS MADE FOR ME");
+            potentialSlot = null;
+        }
+    }
+
+
+}

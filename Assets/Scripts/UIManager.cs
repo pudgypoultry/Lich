67de6a36c2426820fde108d2cc9f3e7c4f742064
Aisling McIntosh <122208641+CriticalSquirrel93@users.noreply.github.com>
2023-05-23 03:51:45 -0700

    Added some functionality and images to UI manager

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,7 +1,8 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using Image = UnityEngine.UI.Image;
 
 public class UIManager : MonoBehaviour
 {
@@ -10,6 +11,13 @@ public class UIManager : MonoBehaviour
     [SerializeField] private GameObject SettingsPanel;
     [SerializeField] private GameObject GameplayPanel;
     [SerializeField] private GameObject PausePanel;
+    [SerializeField] private GameObject CreditsPanel;
+    [SerializeField] private GameObject VictoryPanel;
+    [SerializeField] private GameObject GameOverPanel;
+    
+    [Header("Gameplay Panel Prefabs")]
+    [SerializeField] private GameObject ToolTipPanel;
+    [SerializeField] private GameObject DialoguePanel;
 
     [Header("Settings")]
     [SerializeField] private MenuState initialMenuType = MenuState.Main;
@@ -23,7 +31,10 @@ public class UIManager : MonoBehaviour
         Main,
         Settings,
         Pause,
-        Gameplay
+        Gameplay,
+        Credits,
+        Victory,
+        GameOver,
     }
 
 
@@ -77,15 +88,97 @@ public class UIManager : MonoBehaviour
     {
         if (enabled)
         {
+            if (currentState == MenuState.Pause)
+            {
+                PausePanel.SetActive(false);
+                lastState = MenuState.Pause;
+            }
             SettingsPanel.SetActive(true);
             currentState = MenuState.Settings;
         }
         else
         {
+            Debug.Log(lastState.ToString());
+            if (lastState == MenuState.Pause)
+            {
+                PausePanel.SetActive(true);
+                currentState = MenuState.Pause;
+            }
             SettingsPanel.SetActive(false);
             prefabDictionary[lastState].SetActive(true); // Reopen last panel
             lastState = MenuState.Settings;
         }
+    }
+
+    public void ControlCreditsPanel(bool enabled)
+    {
+        if (enabled)
+        {
+            CreditsPanel.SetActive(true);
+            currentState = MenuState.Credits;
+        }
+        else
+        {
+            CreditsPanel.SetActive(false);
+            lastState = MenuState.Credits;
+        }
+    }
+
+    public void ControlVictoryPanel(bool enabled)
+    {
+        if (enabled)
+        {
+            VictoryPanel.SetActive(true);
+            currentState = MenuState.Victory;
+        }
+        else
+        {
+            VictoryPanel.SetActive(false);
+            lastState = MenuState.Victory;
+        }
+    }
+
+    public void ControlGameOverPanel(bool enabled)
+    {
+        if (enabled)
+        {
+            GameOverPanel.SetActive(true);
+            currentState = MenuState.GameOver;
+        }
+        else
+        {
+            GameOverPanel.SetActive(false);
+            lastState = MenuState.GameOver;
+        }
+    }
+
+    public void DisplayDialogueBox(string message)
+    {
+        StartCoroutine(DisplayDelayedMessage(message));
+    }
+
+    IEnumerator DisplayDelayedMessage(string message)
+    {
+        // Set message
+        DialoguePanel.GetComponentInChildren<TextMeshProUGUI>().text = message;
+        // Fade in the message
+        DialoguePanel.GetComponentInChildren<TextMeshProUGUI>().CrossFadeAlpha(1.0f, 5.0f, false);
+        DialoguePanel.GetComponentInChildren<Image>().CrossFadeAlpha(1.0f, 5.0f, false);
+        // Wait for 5 seconds
+        yield return new WaitForSeconds(5);
+        // Fade out the message
+        DialoguePanel.GetComponentInChildren<Image>().CrossFadeAlpha(0.0f, 5.0f, false);
+        DialoguePanel.GetComponentInChildren<TextMeshProUGUI>().CrossFadeAlpha(0.0f, 5.0f, false);
+        
+        // TODO: Set up a boolean value to prevent multiple dialogues from appearing at once.
+    }
+
+    public void DisplayTooltip(string message)
+    {
+        ToolTipPanel.GetComponentInChildren<TextMeshProUGUI>().text = message;
+        // TODO: Remove these? Instead look into responding to user clicking a card or something
+        DialoguePanel.GetComponentInChildren<Image>().CrossFadeAlpha(1.0f, 5f, false);
+        DialoguePanel.GetComponentInChildren<Image>().CrossFadeAlpha(0.0f, 5f, false);
     }
 
     public void QuitGame()
@@ -111,6 +204,9 @@ public class UIManager : MonoBehaviour
         prefabDictionary[MenuState.Settings] = SettingsPanel;
         prefabDictionary[MenuState.Gameplay] = GameplayPanel;
         prefabDictionary[MenuState.Pause] = PausePanel;
+        prefabDictionary[MenuState.Victory] = VictoryPanel;
+        prefabDictionary[MenuState.GameOver] = GameOverPanel;
+        prefabDictionary[MenuState.Credits] = CreditsPanel;
 
         // Make sure only initial panel is active
         foreach (var panel in prefabDictionary)
